cb16b302d5dc750b7f57c1671a9b7dacace16dc3
shresta <sbalara2@uncc.edu>
2024-05-08 12:20:40 -0400

    final commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+// Footer.js
+
+import React from 'react';
+import '../../styles//Footer.css'; 
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; {new Date().getFullYear()} SHRESTA VARMA. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
